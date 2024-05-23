11a60f4e315c68971cebff70ba43538e5f39e4cb
maroofirfan <maroofirfan@a2kitsolutions.com>
2024-05-23 22:43:22 +0530

    add pre installation

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
 const app = express();
 
 require('dotenv').config()
 const dbConfig = require('./config/dbConfig.js');
+
+const __dirname = path.resolve();
+
 app.use(express.json());
 const userRoute = require('./routes/userRoute.js')
 app.use('/api/user', userRoute);
@@ -11,6 +13,14 @@ const adminRoute = require('./routes/adminRoute.js')
 app.use('/api/admin', adminRoute);
 const doctorRoute = require('./routes/doctorRoute.js')
 app.use('/api/doctor', doctorRoute);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+
 
 const port = process.env.PORT || 5000;
 
